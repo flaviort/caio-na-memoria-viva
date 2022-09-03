3be6f1dab90ae42027a975b62aec34f9f445aa33
Flávio <flavioczuk@gmail.com>
2022-09-03 14:55:24 -0300

    homepage pt1

---
+++ includes/partners.php
@@ -0,0 +1,12 @@
+<section id="partners">
+    <div class="container container-big">
+        <div class="row">
+            <div class="col-12">
+
+                <img src="assets/img/logos-desk.png" alt="Parceiros" class="desk">
+                <img src="assets/img/logos-mob.png" alt="Parceiros" class="mob">
+
+            </div>
+        </div>
+    </div>
+</section>
--- index.php
+++ index.php
@@ -51,7 +51,81 @@
 					</div>
 				</section>
 
-				<section style="width: 100%; height: 50vh; background-color: #ddd; position: relative; z-index: -1; margin-top: 100px"></section>
+				<!-- tour -->
+				<section id="tour">
+					<div class="container container-big">
+						<div class="row">
+
+							<div class="col-md-7 left">
+
+								<img src="assets/img/sunflower.png" alt="Girassol" class="sunflower">
+								<div class="yellow-block"></div>
+								<div class="house"></div>
+
+							</div>
+
+							<div class="col-md-5 right">
+								<div>
+
+									<p class="title-big title">
+										Acesse a <br />
+										<span>Visita Guiada</span>
+									</p>
+
+									<a href="visita-guiada.php" class="black-button text-medium">
+										Visite Agora
+									</a>
+
+								</div>
+							</div>
+
+						</div>
+					</div>
+				</section>
+
+				<!-- featured text -->
+				<section id="featured-text">
+					<div class="container container-big">
+						<div class="row">
+							<div class="col-12">
+
+								<div class="circle"></div>
+								<div class="trapeze"></div>
+								<img src="assets/img/sunflower-blur-01.png" alt="Girassol" class="sunflower">
+
+								<div class="content">
+
+									<p class="text-bigger desc">
+
+										<span class="quote-top">
+											<?php include('assets/svg/quote-top.php') ?>
+										</span>
+
+										A única magia que existe é estarmos vivos e não entendermos nada disso. A única magia que existe é a nossa incompreensão.
+										
+										<span class="quote-bottom">
+											<?php include('assets/svg/quote-bottom.php') ?>
+										</span>
+										
+									</p>
+
+									<p class="text-medium author">
+										Caio Fernando Abreu. Ovelhas Negras - Porto Alegre : Sulina, 1995, p. 118.
+									</p>
+
+								</div>
+
+							</div>
+						</div>
+					</div>
+				</section>
+
+				<!-- news -->
+				<?php include('includes/news.php'); ?>
+
+				<!-- partners -->
+				<?php include('includes/partners.php'); ?>
+
 
 			</main>
 
